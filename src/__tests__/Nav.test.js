34c551e20d9b18ff7d96faadbfe70fa4735c2558
DougCaswell <dougcaswell@gmail.com>
2019-12-17 10:13:00 -0700

    added a little more styling and made it so the current link stands out

--- src/__tests__/Nav.test.js
+++ src/__tests__/Nav.test.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Nav from '../components/Nav.jsx';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('NavBar', () => {
     let mountedNav;
     beforeEach(() => {
-        mountedNav = shallow(<Nav />);
+        mountedNav = mount(
+            <MemoryRouter>
+                <Nav />
+            </MemoryRouter>
+        );
     });
     it('renders without crashing', () => {
         const NavBar = shallow(<Nav />);
     });
     it('renders 3 links', () => {
-        const links = mountedNav.find('Link');
+        const links = mountedNav.find('NavLink');
         expect(links.length).toBe(3);
     });
 });
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 
-const Nav = () => {
+const Nav = (props) => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
-            <Link to="/aboutMe">About Me</Link>
+            <NavLink exact to="/" activeClassName="selected">Home</NavLink>
+            <NavLink to="/projects" activeClassName="selected">Projects</NavLink>
+            <NavLink to="/aboutMe" activeClassName="selected">About Me</NavLink>
         </nav>
     );
 }
